352ea10ec4e7feba2625c1a2df4574fd0e574518
ZairiAimenDz <zairiaimendev@gmail.com>
2020-04-25 19:15:40 +0100

    19:15

--- Doctors_Around/Pages/Index.cshtml.cs
+++ Doctors_Around/Pages/Index.cshtml.cs
@@ -11,10 +11,15 @@ namespace Doctors_Around.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        public string[] Advices = {"KYS","Stay At Home","Cyka" ,"Random Advice Number 3","Generic Advice Number 4" };
+        public string advice;
 
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
+            Random rnd = new Random();
+            int adv = rnd.Next(5);
+            advice = Advices[adv];
         }
 
         public void OnGet()
